7452cb62293fb4bc51a13410edc77bf11ef52926
RafalKielbasa <kielbasarafal92@gmail.com>
2024-06-11 07:44:30 +0200

    quick change

--- server.js
+++ server.js
@@ -36,58 +36,50 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', async (req, res) => {
-  const { question } = req.body
-
-  const isQuestion = await openai.chat.completions
-    .create({
-      messages: [
-        {
-          role: 'user',
-          content: question,
-        },
-        {
-          role: 'system',
-          content: isQuestionMessage,
-        },
-      ],
-      model: 'gpt-4o',
-      response_format: { type: 'json_object' },
-    })
-    .then((data) => data.choices[0].message.content)
-
-  console.log({ isQuestion })
-
-  const isQuestionObject = JSON.parse(isQuestion)
-
-  if (!isQuestionObject.value) {
-    await prisma.info.create({ data: { info: question } })
-    console.log(isQuestionObject)
-  }
-
-  const info = await prisma.info.findMany()
-
-  const dynamicContext = JSON.stringify(info.map(({ info }) => info))
-
-  console.log(dynamicContext)
-
-  const reply = await openai.chat.completions
-    .create({
-      messages: [
-        {
-          role: 'user',
-          content: question,
-        },
-        {
-          role: 'system',
-          content: ` ${systemMessage} context###${dynamicContext}###`,
-        },
-      ],
-      model: 'gpt-4o',
-    })
-    .then((data) => data.choices[0].message.content)
-
-  // // Wyślij odpowiedź w formacie JSON
-  res.json({ reply })
+  // const { question } = req.body
+  // const isQuestion = await openai.chat.completions
+  //   .create({
+  //     messages: [
+  //       {
+  //         role: 'user',
+  //         content: question,
+  //       },
+  //       {
+  //         role: 'system',
+  //         content: isQuestionMessage,
+  //       },
+  //     ],
+  //     model: 'gpt-4o',
+  //     response_format: { type: 'json_object' },
+  //   })
+  //   .then((data) => data.choices[0].message.content)
+  // console.log({ isQuestion })
+  // const isQuestionObject = JSON.parse(isQuestion)
+  // if (!isQuestionObject.value) {
+  //   await prisma.info.create({ data: { info: question } })
+  //   console.log(isQuestionObject)
+  // }
+  // const info = await prisma.info.findMany()
+  // const dynamicContext = JSON.stringify(info.map(({ info }) => info))
+  // console.log(dynamicContext)
+  // const reply = await openai.chat.completions
+  //   .create({
+  //     messages: [
+  //       {
+  //         role: 'user',
+  //         content: question,
+  //       },
+  //       {
+  //         role: 'system',
+  //         content: ` ${systemMessage} context###${dynamicContext}###`,
+  //       },
+  //     ],
+  //     model: 'gpt-4o',
+  //   })
+  //   .then((data) => data.choices[0].message.content)
+  // // // Wyślij odpowiedź w formacie JSON
+  // res.json({ reply })
+  console.log({ req })
 })
 
 app.listen(3000)
